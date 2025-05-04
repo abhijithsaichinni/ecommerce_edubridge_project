356182b7383522aa2c904a3de728fc8b25522d53
Abhijith Sai Chinni <99478457+abhijithsaichinni@users.noreply.github.com>
2025-05-04 22:18:41 +0530

    Delete Ecommerce directory

--- Ecommerce/src/main/java/com/example/spring/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.spring.model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin , Integer> {
-
-}
--- Ecommerce/src/main/java/com/example/spring/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.spring.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
